66fc5a87768dc3bc1edaf4ce8769a7e2c673ff75
Bap <yenoanhpcg@gmail.com>
2025-05-29 21:51:39 +0700

    update login and sign up

--- project/src/app/Header/index.tsx
+++ project/src/app/Header/index.tsx
@@ -1,4 +1,15 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import QC from "../../../public/img/QC.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faPhone, faUser } from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
   return (
     <>
@@ -49,7 +60,196 @@ export default function Header() {
           </ul>
         </div>
         <div>
-          <a
+          <Dialog>
+            <DialogTrigger className="text-main px-[15px] py-[5px] mr-3 rounded-md leading-5 text-2xl font-medium hover:text-[#fff] hover:bg-main">
+              Đăng nhập
+            </DialogTrigger>
+            <DialogContent className="w-250">
+              <DialogHeader>
+                <DialogTitle className="text-2xl text-center font-bold text-main">
+                  Đăng nhập tài khoản
+                </DialogTitle>
+                <DialogDescription>
+                  <div>
+                    <span className="font-bold">Đăng nhập với tư cách</span>
+                    <select
+                      name=""
+                      className="w-full h-8 bg-bg mt-4 px-2.5 pl-11 rounded-[6.6px] border-none"
+                    >
+                      <option value=""></option>
+                      <option value="">Khách hàng</option>
+                      <option value="">Nhà cung cấp</option>
+                      <option value="">Nhân viên bán hàng</option>
+                      <option value="">Nhân viên kho</option>
+                    </select>
+                    <br />
+                    <span className="font-bold">Thông tin đăng nhập</span>
+                    <form action="">
+                      <div className="relative">
+                        <FontAwesomeIcon
+                          className="text-main absolute top-1/2 ml-3.5"
+                          icon={faPhone}
+                        />
+                        <i className=" fa-solid "></i>
+                        <input
+                          className="w-full h-8 bg-bg mt-4 px-2.5 pl-11 rounded-[6.6px] border-none"
+                          type="text"
+                          placeholder="Số điện thoại của bạn"
+                        />
+                      </div>
+                      <div className="relative">
+                        <FontAwesomeIcon
+                          icon={faLock}
+                          className="text-main absolute top-1/2 ml-3.5"
+                        />
+                        <input
+                          className="w-full h-8 bg-bg mt-4 px-2.5 pl-11 rounded-[6.6px] border-none"
+                          type="text"
+                          placeholder="Nhập mật khẩu"
+                        />
+                      </div>
+                      <a href="#" className="text-main my-3 float-right">
+                        Quên mật khẩu?
+                      </a>
+                      <br />
+                      <span>
+                        <input
+                          type="checkbox"
+                          className="form_signup_wrap_icon-input-agree"
+                        />{" "}
+                        Tôi đồng ý với{" "}
+                        <a
+                          href="https://thuocsi.vn/terms-and-condition"
+                          target="_blank"
+                          className="underline font-medium"
+                        >
+                          {" "}
+                          Điều khoản sử dụng
+                        </a>{" "}
+                        và xác nhận tôi đã có đủ giấy phép cần thiết cũng như
+                        đáp ứng các điều kiện theo quy định của pháp luật để mua
+                        sản phẩm trên thuocsi.vn khi đăng ký tài khoản.
+                      </span>
+                      <br />
+                      <a
+                        href="#"
+                        className="flex m-auto bg-main text-[#fff] w-full h-10 font-bold justify-center items-center text-xl no-underline mt-2 rounded-[6.6px]"
+                      >
+                        Đăng nhập
+                      </a>
+                    </form>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          <Dialog>
+            <DialogTrigger className="bg-main text-[#fff] px-[15px] py-[5px] w-36 h-10 rounded-md leading-5 text-2xl font-medium hover:text-main hover:bg-[#fff]">
+              Đăng ký
+            </DialogTrigger>
+            <DialogContent className="w-250">
+              <DialogHeader>
+                <DialogTitle className="text-2xl text-center font-bold text-main">
+                  Đăng ký tài khoản
+                </DialogTitle>
+                <DialogDescription>
+                  <div>
+                    <span className="font-bold">
+                      Vui lòng chọn đối tượng đăng ký
+                    </span>
+                    <select
+                      name=""
+                      className="w-full h-8 bg-bg mt-4 px-2.5 pl-11 rounded-[6.6px] border-none"
+                    >
+                      <option value=""></option>
+                      <option value="">Khách hàng</option>
+                      <option value="">Nhà cung cấp</option>
+                      <option value="">Nhân viên bán hàng</option>
+                      <option value="">Nhân viên kho</option>
+                    </select>
+                    <br />
+                    <span>Thông tin đăng ký</span>
+                    <form action="">
+                      <div className="relative">
+                        <FontAwesomeIcon
+                          icon={faUser}
+                          className="text-main absolute top-1/2 ml-3.5"
+                        />
+                        <input
+                          className="w-full h-8 bg-bg mt-4 px-2.5 pl-11 rounded-[6.6px] border-none"
+                          type="text"
+                          placeholder="Họ và tên"
+                        />
+                      </div>
+                      <div className="relative">
+                        <FontAwesomeIcon
+                          className="text-main absolute top-1/2 ml-3.5"
+                          icon={faPhone}
+                        />
+                        <i className=" fa-solid "></i>
+                        <input
+                          className="w-full h-8 bg-bg mt-4 px-2.5 pl-11 rounded-[6.6px] border-none"
+                          type="text"
+                          placeholder="Số điện thoại của bạn"
+                        />
+                      </div>
+                      <div className="relative">
+                        <FontAwesomeIcon
+                          icon={faLock}
+                          className="text-main absolute top-1/2 ml-3.5"
+                        />
+                        <input
+                          className="w-full h-8 bg-bg mt-4 px-2.5 pl-11 rounded-[6.6px] border-none"
+                          type="text"
+                          placeholder="Nhập mật khẩu"
+                        />
+                      </div>
+                      <div className="relative">
+                        <FontAwesomeIcon
+                          icon={faLock}
+                          className="text-main absolute top-1/2 ml-3.5 "
+                        />
+                        <input
+                          className="w-full h-8 bg-bg mt-4 px-2.5 pl-11 rounded-[6.6px] border-none"
+                          type="text"
+                          placeholder="Nhập lại mật khẩu"
+                        />
+                      </div>
+                      <br />
+                      <span>
+                        <input
+                          type="checkbox"
+                          className="form_signup_wrap_icon-input-agree"
+                        />{" "}
+                        Tôi đồng ý với{" "}
+                        <a
+                          href="https://thuocsi.vn/terms-and-condition"
+                          target="_blank"
+                          className="underline font-medium"
+                        >
+                          {" "}
+                          Điều khoản sử dụng
+                        </a>{" "}
+                        và xác nhận tôi đã có đủ giấy phép cần thiết cũng như
+                        đáp ứng các điều kiện theo quy định của pháp luật để mua
+                        sản phẩm trên thuocsi.vn khi đăng ký tài khoản.
+                      </span>
+                      <br />
+                      <a
+                        href="#"
+                        className="flex m-auto bg-main text-[#fff] w-full h-10 font-bold justify-center items-center text-xl no-underline mt-2 rounded-[6.6px]"
+                      >
+                        Đăng ký
+                      </a>
+                    </form>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          {/* <a
             href="#"
             className="text-main px-[15px] py-[5px] mr-3 rounded-md leading-5 text-2xl font-medium hover:text-[#fff] hover:bg-main"
           >
@@ -60,7 +260,7 @@ export default function Header() {
             className="bg-main text-[#fff] px-[15px] py-[5px] rounded-md leading-5 text-2xl font-medium hover:text-main hover:bg-[#fff]"
           >
             Đăng ký
-          </a>
+          </a> */}
         </div>
       </div>
     </>
--- project/src/app/Keyword/index.tsx
+++ project/src/app/Keyword/index.tsx
@@ -1,89 +1,51 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
-const invoices = [
+const Data = [
   {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
+    text_1: "Cà phê bột",
+    text_2: "Sỉ cà phê số lượng lớn",
+    text_3: "Cà phê sỉ Bình Dương",
+    text_4: "Cà phê giá rẻ",
+    text_5: "Cà phê rang xay",
+    text_6: "Cà phê chất lượng",
+    text_7: "Cà phê sữa",
+    text_8: "Cà phê củi",
   },
 ];
 export default function Keyword() {
   return (
     <>
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
-          </TableRow>
-        </TableHeader>
+      <Table className="flex justify-center mx-auto my-3 text-[#5b5b5b] w-full">
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">
-                {invoice.totalAmount}
+          {Data.map((text_1) => (
+            <TableRow key={text_1.text_1}>
+              <TableCell className="border-none px-2 py-1">
+                {text_1.text_1}
+              </TableCell>
+              <TableCell className="border-none px-2 py-1">
+                {text_1.text_2}
+              </TableCell>
+              <TableCell className="border-none px-2 py-1">
+                {text_1.text_3}
+              </TableCell>
+              <TableCell className="border-none px-2 py-1">
+                {text_1.text_4}
+              </TableCell>
+              <TableCell className="border-none px-2 py-1">
+                {text_1.text_5}
+              </TableCell>
+              <TableCell className="border-none px-2 py-1">
+                {text_1.text_6}
+              </TableCell>
+              <TableCell className="border-none px-2 py-1">
+                {text_1.text_7}
+              </TableCell>
+              <TableCell className="border-none px-2 py-1">
+                {text_1.text_8}
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
-        </TableFooter>
       </Table>
       {/* <div className="flex flex-col text-center overflow-scroll text-nowrap">
         <table className="flex justify-center mx-auto my-3 text-[#5b5b5b] w-full">
