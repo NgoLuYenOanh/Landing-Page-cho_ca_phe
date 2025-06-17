fcbb4f84e716932b4fa8231ad2c71ae2a2e9821a
Bap <yenoanhpcg@gmail.com>
2025-06-17 14:30:27 +0700

    feat: test API

--- project/next.config.ts
+++ project/next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
--- project/src/app/(auth)/Page_post_news/Post_news_left/components/index.tsx
+++ project/src/app/(auth)/Page_post_news/Post_news_left/components/index.tsx
@@ -1,35 +1,41 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 interface IPost_left {
   id: number;
-  img: string;
+  url: string;
   title: string;
-  paragraph: string;
-}
-interface Post_left_list_props {
-  post: IPost_left[];
 }
 
-export default function Post_left({ post }: Post_left_list_props) {
+export default function Post_left() {
+  const [photos, setPhotos] = useState<IPost_left[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos?_limit=10")
+      .then((res) => res.json())
+      .then((data: IPost_left[]) => setPhotos(data));
+  }, []);
+
   return (
     <>
       <div className="max-lg:w-8/10 max-lg:justify-center w-7/10 flex flex-col gap-6 mb-5">
-        {post.map((item) => (
+        {photos.map(({ id, url, title }) => (
           <div
-            key={item.id}
+            key={id}
             className="flex flex-row max-sm:flex-col gap-4 bg-[#fff] rounded-3xl"
           >
             <Image
-              src={item.img}
-              className="rounded-3xl"
+              src={url}
+              className="rounded-3xl object-cover"
               alt=""
               width={371}
               height={208}
             />
             <div className="">
               <a href="" className="text-2xl hover:text-main">
-                {item.title}
+                {title}
               </a>
-              <p className="text-[#999999] ">{item.paragraph}</p>
+              {/* <p className="text-[#999999] ">{paragraph}</p> */}
             </div>
           </div>
         ))}
